55f35da357fc45684848d297a8315200d64dce61
Hanif Bahari <hanifbahari@Hanifs-MacBook-Pro-2.local>
2023-02-14 19:26:19 +0100

    main component

---
+++ components/Main.jsx
@@ -0,0 +1,52 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+import { AiOutlineMail } from "react-icons/ai";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+
+const Main = () => {
+  return (
+    <div className="w-full h-screen text-center">
+      <div className="max-w-[1240px w-full h-full mx-auto p-2 flex justify-center items-center">
+        <div>
+          <p className="uppercase text-sm tracking-wildest text-gray-600">
+            LET'S BUILD SOMETHING TOGETHER
+          </p>
+          <h1 className="py-4 text-gray-700">
+            Hi, I'm <span className="text-[#1e81b0]"> Hanif</span>
+          </h1>
+          <h1 className="py-2 text-gray-700">
+            A Front-end Developer
+          </h1>
+          <h2 className="py-2 text-gray-700 font-normal">with a background in Design</h2>
+          <p className="py-4 text-gray-500 max-w-[70%] m-auto">
+            As a front-end web developer with a design background, I bring a
+            unique combination of technical and creative skills to every
+            project. With a passion for creating intuitive user experiences and
+            visually stunning interfaces, I strive to bring the best of both
+            worlds to the table. From conceptualization to implementation, I
+            approach every project with an eye for detail and a commitment to
+            excellence.
+          </p>
+          <div className="flex items-center justify-between max-w-[330px] m-auto py-4">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300">
+                <FaLinkedinIn />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300">
+                <FaGithub />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300">
+                <AiOutlineMail />
+            </div>
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-105 ease-in duration-300">
+                <BsFillPersonLinesFill />
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -99,10 +99,7 @@ function Navbar() {
               </Link>
             </ul>
             <div className="pt-40">
-              <p className="uppercase tracking-widest text-[#5651e5]">
-                {" "}
-                Let's Connect{" "}
-              </p>
+              
               <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
                 <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300 ">
                   <FaLinkedinIn />
