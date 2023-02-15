bef9745c7d09653ba1495e20a034e41c0d2563e6
Hanif Bahari <hanifbahari@Hanifs-MacBook-Pro-2.local>
2023-02-15 07:59:31 +0100

    skills component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import About from '@/components/About'
 import Main from '@/components/Main'
 import Navbar from '@/components/Navbar'
+import Skills from '@/components/Skills'
 import Head from 'next/head'
 
 
@@ -18,6 +19,7 @@ export default function Home() {
       <Navbar />
      <Main/>
      <About />
+     <Skills />
     </>
   )
 }
