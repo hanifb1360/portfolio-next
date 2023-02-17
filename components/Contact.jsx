d96fab4055ca65266828236b17f64529bbc1b31d
Hanif Bahari <hanifbahari@Hanifs-MacBook-Pro-2.local>
2023-02-17 11:31:02 +0100

    email.js contact

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,21 +1,47 @@
-/* eslint-disable react/no-unescaped-entities */
-import React from 'react'
 
-const Contact = () => {
-  return (
-    <div id='contact' className='w-full h-screen  flex justify-center items-center p-4'>
-        <form method='POST' action="https://getform.io/f/a699a1b2-f225-434e-b317-1fbbde8e006c" className='flex flex-col max-w-[600px] w-full'>
-            <div className='pb-8'>
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.form = React.createRef();
+  }
+
+  sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_bkbxe8n', 'template_ihrvx2e', this.form.current, 'AGC5Umln9-uNz59rD')
+      .then((result) => {
+          console.log(result.text);
+          console.log('message sent');
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  render() {
+    return (
+      <div id='contact' className='w-full h-screen  flex justify-center items-center p-4'>
+      <form ref={this.form} onSubmit={this.sendEmail} className='flex flex-col max-w-[600px] w-full'>
+        <div className='pb-8'>
                 <p className='text-4xl font-bold inline  text-[#3a81cd]'>Contact</p>
                 
-            </div>
-            <input className='border-2 border-gray-300 px-4 py-2 rounded-full' type="text" placeholder='Name' name='name' />
-            <input className='border-2 border-gray-300 px-4 py-2 rounded-full my-4' type="email" placeholder='Email' name='email' />
-            <textarea className='border-2 border-gray-300 rounded-lg px-4 py-2' name="message" rows="10" placeholder='Message'></textarea>
-            <button className="transition duration-300 ease-in-out w-full p-4 text-gray-100 mt-4 bg-[#3a81cd] hover:bg-cyan-600">SEND</button>
-        </form>
-    </div>
-  )
+           </div>
+       
+        <input placeholder='Name' className='border-2 border-gray-300 px-4 py-2 rounded-full' type="text" name="user_name" />
+       
+        <input placeholder='Email' className='border-2 border-gray-300 px-4 py-2 rounded-full my-4' type="email" name="user_email" />
+       
+        <textarea placeholder='Message' className='border-2 border-gray-300 rounded-lg px-4 py-2' name="message" rows="10" />
+        <input className="transition duration-300 ease-in-out w-full p-4 text-gray-100 mt-4 bg-[#3a81cd] hover:bg-cyan-600" type="submit" value="Send" />
+      </form>
+      </div>
+    );
+  }
 }
 
-export default Contact
+export default Contact;
+
+
+
